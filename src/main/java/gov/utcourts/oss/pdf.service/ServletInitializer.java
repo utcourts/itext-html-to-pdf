6efb30c4b5be00be30aa154698b0425bf67ff6c5
Ramakrishnan Kuppusami <Ramakrishnan.kuppusami@cdw.com>
2025-09-09 14:51:38 +0530

    UTCOURT-18316: Add foundation files

---
+++ src/main/java/gov/utcourts/oss/pdf.service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package gov.utcourts.oss.pdf.service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PdfServiceApplication.class);
+	}
+
+}
---
+++ src/main/java/gov/utcourts/oss/pdf/service/rest/PdfService.java
@@ -0,0 +1,8 @@
+package gov.utcourts.oss.pdf.service.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PdfService {
+
+}
